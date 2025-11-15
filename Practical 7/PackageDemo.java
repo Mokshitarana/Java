9609fc29bf394b4cde85c14c29d86814bedd3fd0
Mokshitarana <mokshitarana06@gmail.com>
2025-11-15 19:24:07 +0530

    Add files via upload

---
+++ Practical 7/PackageDemo.java
@@ -0,0 +1,13 @@
+package exercise.corejava;
+
+public class PackageDemo {
+    // public static so it can be called directly without new
+    public static void display() {
+        System.out.println("PackageDemo executed");
+    }
+
+    // optional: instance method example
+    public void displayInstance() {
+        System.out.println("PackageDemo instance method executed");
+    }
+}
